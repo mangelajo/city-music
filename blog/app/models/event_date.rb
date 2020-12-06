a2271e193aedd684fc2fd8666d1698ada5f403cd
Miguel Angel Ajo <majopela@redhat.com>
2020-12-06 14:41:32 +0100

    Event Date month as 3 letter upcase

--- blog/app/models/event_date.rb
+++ blog/app/models/event_date.rb
@@ -4,4 +4,9 @@ class EventDate < ApplicationRecord
   def to_s
     date.to_s
   end
+
+  def month_s
+    long = I18n.t('date.month_names')[date.month]
+    return long[0..2].upcase
+  end
 end
--- blog/test/models/event_date_test.rb
+++ blog/test/models/event_date_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class EventDateTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "month_s method returns 3 letter upcase month" do
+    assert event_dates(:one).month_s == "DEC"
+    assert event_dates(:two).month_s == "JAN"
+  end
 end
