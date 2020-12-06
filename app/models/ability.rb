74a96720c035195f75e29aea60e6673391c9859a
Miguel Angel Ajo <majopela@redhat.com>
2020-12-06 15:24:36 +0100

    Rename to CityMusic and enable rails-admin auth

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,10 +2,12 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
+    pp user
     user ||= User.new # guest user (not logged in)
     if user && user.admin?
-      can :access, :rails_admin       # only allow admin users to access Rails Admin
       can :manage, :all
+      can :access, :rails_admin       # only allow admin users to access Rails Admin
+      can :read, :dashboard
     else
       can :read, :all                   # allow everyone to read everything
     end
