3fa97df109de85b60e27fd4e435fe4c4f45db2d9
Miguel Angel Ajo <majopela@redhat.com>
2020-12-07 15:56:39 +0100

    If event has no picture, use band's one

--- app/models/event.rb
+++ app/models/event.rb
@@ -5,4 +5,12 @@ class Event < ApplicationRecord
   has_many :event_dates, :dependent => :destroy
 
   has_and_belongs_to_many :genres
+
+  def image
+    if images.attached?
+      return images[0]
+    elsif band.images.attached?
+      return band.images[0]
+    end
+  end
 end
--- test/models/event_test.rb
+++ test/models/event_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class EventTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "if event has not picture, it will use band's picture" do
+
+    event = Event.first
+
+    assert_nil event.image
+
+    event.band.images.attach(io: File.open("#{Rails.root}/test/fixtures/files/band.jpg"), filename: 'band.jpg',
+                             content_type: 'image/jpg')
+
+    assert event.image == event.band.images[0]
+
+    event.images.attach(io: File.open("#{Rails.root}/test/fixtures/files/event.jpg"), filename: 'event.jpg',
+                             content_type: 'image/jpg')
+    assert event.image == event.images[0]
+
+  end
 end
